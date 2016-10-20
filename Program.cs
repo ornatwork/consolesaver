2799a17d8e2ab7fd8346d4cfae6a1b7f2d685c22
ornatwork <ornsmail@gmail.com>
2016-10-20 15:19:24 -0600

    Added main project and code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleSaver
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            // Loop for a while 
+            for(int i=0; i<10000; i++)
+            {               
+                String tmp = "";
+                int current = rnd.Next(180);
+                // Fill and display    
+                tmp = tmp.PadRight( current, ' ') + '|';
+                Console.WriteLine(tmp);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
